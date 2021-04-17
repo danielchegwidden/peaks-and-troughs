210b89b39b2ed2cd85bb4fbecb88a1ff579779a7
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-04-17 17:25:31 +0800

    navigation and content tabs

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("learn"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -32,7 +32,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
-            next_page = url_for("index")
+            next_page = url_for("learn")
         return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
@@ -46,7 +46,7 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for("learn"))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
