5726ae326bb865016267887adc1e71d57d56ed6f
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-04-28 21:27:56 +0800

    fix models

--- database_test.py
+++ database_test.py
@@ -8,18 +8,12 @@
 # db.session.add(admin)
 # db.session.commit()
 
-# me = Attempt(
-#     user_id=99, bracket="High", question_1=1, question_2=1, question_3=0, question_4=0, question_5=1
-# )
-# me.post_score(me.bracket, me.question_1, me.question_2, me.question_3, me.question_4, me.question_5)
-# me.generate_score(
-#     scores=[me.question_1, me.question_2, me.question_3, me.question_4, me.question_5]
-# )
+# me = Attempt(id=99, user_id=99, category="High", question_1=1, question_2=1, question_3=0, question_4=0, question_5=1)
+# me.post_score(me.category, me.question_1, me.question_2, me.question_3, me.question_4, me.question_5)
+# me.generate_score(scores=[me.question_1, me.question_2, me.question_3, me.question_4, me.question_5])
 # db.session.add(me)
 # db.session.commit()
 
-# prog = Progress(
-#     user_id=99, high_a=1, high_b=1, high_c=1, high_d=0, low_a=0, low_b=0, low_c=0, low_d=0
-# )
+# prog = Progress(user_id=99, high_a=1, high_b=1, high_c=1, high_d=0, low_a=0, low_b=0, low_c=0, low_d=0)
 # db.session.add(prog)
 # db.session.commit()
