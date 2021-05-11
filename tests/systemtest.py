436f574ece1d403c9736d670cfd52116612f4bff
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2021-05-11 21:16:50 +0800

    system testing

--- tests/systemtest.py
+++ tests/systemtest.py
@@ -3,6 +3,7 @@ from app import app, db
 from app.models import Users, Progress, Attempt, Questions
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -21,7 +22,7 @@ class AccessSystemTest(unittest.TestCase):
             db.init_app(app)
             db.create_all()
             u1 = Users(id=9999, username="FirstSysTest", email="test-s1@peaksandtroughs.com")
-            u1.set_password("systemtest1")
+            u1.set_password("systemTest1!")
             db.session.add(u1)
             db.session.commit()
             self.driver.maximize_window()
@@ -34,32 +35,36 @@ class AccessSystemTest(unittest.TestCase):
             db.drop_all()
 
     def test_register_and_login(self):
+        # Check user is not in the database
         u = Users.query.get(9999)
         self.assertEqual(u.username, "FirstSysTest", msg="user does not exist in database")
 
         self.driver.get("http://localhost:5000/register")
         self.driver.implicitly_wait(5)
 
+        # Register new user
         username_field = self.driver.find_element_by_id("username")
         username_field.send_keys("SecondSysTest")
         email_field = self.driver.find_element_by_id("email")
         email_field.send_keys("test-s2@peaksandtroughs.com")
         password_field = self.driver.find_element_by_id("password")
-        password_field.send_keys("systemtest2")
+        password_field.send_keys("systemTest2!")
         password2_field = self.driver.find_element_by_id("password2")
-        password2_field.send_keys("systemtest2")
+        password2_field.send_keys("systemTest2!")
         register = self.driver.find_element_by_id("submit")
         register.click()
         self.driver.implicitly_wait(5)
 
+        # Login with new user
         username_field = self.driver.find_element_by_id("username")
         username_field.send_keys("SecondSysTest")
         password_field = self.driver.find_element_by_id("password")
-        password_field.send_keys("systemtest2")
+        password_field.send_keys("systemTest2!")
         login = self.driver.find_element_by_id("submit")
         login.click()
         self.driver.implicitly_wait(5)
 
+        # Check that the user is logged in and can logout
         nav_logout = self.driver.find_element_by_id("nav_logout")
         self.assertEqual(nav_logout.get_attribute("innerHTML"), "Logout", msg="user not logged in")
 
@@ -68,14 +73,16 @@ class AccessSystemTest(unittest.TestCase):
         self.driver.get("http://localhost:5000/login")
         self.driver.implicitly_wait(5)
 
+        # Login user
         username_field = self.driver.find_element_by_id("username")
         username_field.send_keys(u.username)
         password_field = self.driver.find_element_by_id("password")
-        password_field.send_keys("systemtest1")
+        password_field.send_keys("systemTest1!")
         login = self.driver.find_element_by_id("submit")
         login.click()
         self.driver.implicitly_wait(5)
 
+        # Check that the user is logged in and can logout
         nav_logout = self.driver.find_element_by_id("nav_logout")
         self.assertEqual(nav_logout.get_attribute("innerHTML"), "Logout", msg="user not logged in")
 
@@ -84,14 +91,16 @@ class AccessSystemTest(unittest.TestCase):
         self.driver.get("http://localhost:5000/login")
         self.driver.implicitly_wait(5)
 
+        # Login with user with incorrect credentials
         username_field = self.driver.find_element_by_id("username")
         username_field.send_keys(u.username)
         password_field = self.driver.find_element_by_id("password")
-        password_field.send_keys("systemtest2")
+        password_field.send_keys("systemTest2!")
         login = self.driver.find_element_by_id("submit")
         login.click()
         self.driver.implicitly_wait(5)
 
+        # Check that the user is not logged in
         nav_login = self.driver.find_element_by_id("nav_login")
         self.assertEqual(nav_login.get_attribute("innerHTML"), "Login", msg="user has logged in")
 
@@ -103,7 +112,7 @@ class NavigationSystemTest(unittest.TestCase):
         self.driver = webdriver.Firefox(executable_path=os.path.join(basedir, "geckodriver"))
 
         uname = "FirstSysTest"
-        pwd = "systemtest1"
+        pwd = "systemTest1!"
 
         if not self.driver:
             self.skipTest("Web browser is not available")
@@ -111,8 +120,10 @@ class NavigationSystemTest(unittest.TestCase):
             db.init_app(app)
             db.create_all()
             u1 = Users(id=9999, username=uname, email="test-s1@peaksandtroughs.com")
+            progress = Progress(user_id=9999)
             u1.set_password(pwd)
             db.session.add(u1)
+            db.session.add(progress)
             db.session.commit()
             self.driver.maximize_window()
             self.driver.get("http://localhost:5000/login")
@@ -136,27 +147,63 @@ class NavigationSystemTest(unittest.TestCase):
         self.driver.get("http://localhost:5000/learn")
         self.driver.implicitly_wait(5)
 
+        p1 = Progress.query.filter_by(user_id=u.id).first()
+        self.assertFalse(p1.high_a)
+
+        # Navigate to High Risk content
         high = self.driver.find_element_by_id("high")
         high.click()
         self.driver.implicitly_wait(5)
 
-        # NEED TO DO PROGRESS UPDATE HERE
+        # Naviagte to Stocks content
+        high_a_box = self.driver.find_element_by_id("high_a_box")
+        high_a_box.click()
+        self.driver.implicitly_wait(5)
 
-    def test_attempt_assessment(self):
+        # Scroll through content and submit progress feedback for Stocks content
+        high_a = self.driver.find_element_by_id("high_a")
+        high_a.send_keys(Keys.PAGE_DOWN)
+        high_a.click()  # THIS FAILS FOR SOME REASON
+        self.driver.implicitly_wait(5)
+
+        # Check that progress has been reflected in database
+        p2 = Progress.query.filter_by(user_id=u.id).first()
+        self.assertTrue(p2.high_a)  # NOT CORRECT RESULT
+
+    def test_access_feedback_locked(self):
+        u = Users.query.get(9999)
+        self.driver.get("http://localhost:5000/feedback")
+        self.driver.implicitly_wait(5)
+
+        # Attempt to navigate to feedback
+        feedback = self.driver.find_element_by_id("feedback")
+        feedback.click()
+        self.driver.implicitly_wait(5)
+
+        # Check to see if still on index page
+        feedback2 = self.driver.find_element_by_id("feedback")
+        self.assertEqual(
+            feedback2.get_attribute("innerHTML"), "Feedback", msg="no longer on index page"
+        )
+
+    def test_assessment_and_feedback(self):
         u = Users.query.get(9999)
         self.driver.get("http://localhost:5000/assessment")
         self.driver.implicitly_wait(5)
 
+        # Submit assessment with no answers
         submit = self.driver.find_element_by_id("submit")
         submit.click()
         self.driver.implicitly_wait(5)
 
+        # Check to see still at the assessment page
         page = self.driver.find_element_by_id("assessment_title")
         self.assertEqual(
             page.get_attribute("innerHTML"), "Complete Assessment", msg="no longer at assessment"
         )
         self.driver.implicitly_wait(5)
 
+        # Select assessment answers
         select_1 = Select(self.driver.find_element_by_id("answer_1"))
         select_1.select_by_value("A")
         select_2 = Select(self.driver.find_element_by_id("answer_2"))
@@ -170,19 +217,16 @@ class NavigationSystemTest(unittest.TestCase):
         submit.click()
         self.driver.implicitly_wait(5)
 
-        a = Attempt.query.filter_by(user_id=u.id).first()
-        self.assertTrue(a.id)  # NOT WORKING
-
-    def test_access_feedback(self):
-        u = Users.query.get(9999)
-        self.driver.get("http://localhost:5000/feedback")
+        # Navigate to Feedback
+        self.driver.get("http://localhost:5000/index")
         self.driver.implicitly_wait(5)
 
-        # SOMETHING TO SHOW THAT ON INDEX PAGE
+        feedback = self.driver.find_element_by_id("feedback")
+        feedback.click()
+        self.driver.implicitly_wait(5)
 
-        # SUBMIT ASSESSMENT
-
-        # VIEW FEEDBACK
+        # Check to see if on feedback page with latest results
+        self.assertTrue(self.driver.find_element_by_id("table_results"))
 
 
 if __name__ == "__main__":
